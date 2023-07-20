bfe41d73c57ceff1146dd39ae88b2edb775725bc
Ajay Pratap Singh <108774300+ajaypratap01@users.noreply.github.com>
2023-07-20 14:53:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ let newarray = Array.from(buttons);
 newarray.forEach(element => {
   element.addEventListener("click", (e) => {
     if (e.target.innerHTML === "=") {
-      blank = eval(blank);
+     if(input.value==="" || input.value==="undefined" || /[a-zA-Z]/.test(input.value)){
+        alert("please enter a valid number")
+      }else{
+        blank = eval(blank);
       input.value = blank;
+      }
     } else if (e.target.innerHTML === "ac" || e.target.innerHTML === "c") {
       blank = "";
       input.value = blank;
